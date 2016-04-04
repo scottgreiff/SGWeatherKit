0b0bca1dfe5d01919f946420814c902fca50a5dd
Scott Alan Greiff <scott@greiff.org>
2016-04-04 08:47:34 -0400

    Weather SDK initial project setup.

---
+++ SGWeatherKit/SGWeatherKit.h
@@ -0,0 +1,19 @@
+//
+//  SGWeatherKit.h
+//  SGWeatherKit
+//
+//  Created by Scott Alan Greiff on 4/4/16.
+//  Copyright © 2016 Scott Alan Greiff. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SGWeatherKit.
+FOUNDATION_EXPORT double SGWeatherKitVersionNumber;
+
+//! Project version string for SGWeatherKit.
+FOUNDATION_EXPORT const unsigned char SGWeatherKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SGWeatherKit/PublicHeader.h>
+
+
---
+++ SGWeatherKitTests/SGWeatherKitTests.swift
@@ -0,0 +1,36 @@
+//
+//  SGWeatherKitTests.swift
+//  SGWeatherKitTests
+//
+//  Created by Scott Alan Greiff on 4/4/16.
+//  Copyright © 2016 Scott Alan Greiff. All rights reserved.
+//
+
+import XCTest
+@testable import SGWeatherKit
+
+class SGWeatherKitTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
