cceb0768e4a2d17f448520c8d51a9c1c45a23d7e
Scott Alan Greiff <scott@greiff.org>
2016-04-05 23:15:09 -0400

    Added city lat/lon to support map overlays.

--- SGWeatherKit/City.swift
+++ SGWeatherKit/City.swift
@@ -11,8 +11,10 @@ import Foundation
 public class City {
     public var name: String!
     public var weatherList: [WeatherListItem]?
+    public var lat: Double!
+    public var lon: Double!
 
-    init(name: String, weatherList: [WeatherListItem]) {
+    init(name: String, lat: Double, lon: Double, weatherList: [WeatherListItem]) {
         self.name = name
         self.weatherList = weatherList
     }
@@ -22,9 +24,16 @@ extension City {
     class func parseFromDictionary(dict: Dictionary<String, AnyObject>) throws -> City {
         var weatherList: [WeatherListItem] = [WeatherListItem]()
         var name: String!
+        var lat: Double!
+        var lon: Double!
 
         if let cityName = dict["name"] as? String {
             name = cityName
+            
+            if let coords = dict["coord"] as? Dictionary<String, AnyObject> {
+                lat = coords["lat"] as? Double
+                lon = coords["lon"] as? Double
+            }
 
             // create singular weather list item
             
@@ -33,6 +42,11 @@ extension City {
 
         } else if let cityDict = dict["city"] as? Dictionary<String, AnyObject> {
             name = cityDict["name"] as? String
+
+            if let coords = cityDict["coord"] as? Dictionary<String, AnyObject> {
+                lat = coords["lat"] as? Double
+                lon = coords["lon"] as? Double
+            }
 
             // iterate list items
             if let weatherArray = dict["list"] as? Array<Dictionary<String, AnyObject>> {
@@ -44,8 +58,10 @@ extension City {
         }
         
         guard let _ = name else { throw ParseError.MissingDictionaryElement }
+        guard let _ = lat else { throw ParseError.MissingDictionaryElement }
+        guard let _ = lon else { throw ParseError.MissingDictionaryElement }
         guard weatherList.count > 0 else { throw ParseError.MissingDictionaryElement }
         
-        return City(name: name, weatherList: weatherList)
+        return City(name: name, lat: lat, lon: lon, weatherList: weatherList)
     }
 }
